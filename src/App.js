1e5838d8bf8144421a90d7c4787c8516ee1729db
taslimahossain <htaslima.outlook.com>
2022-01-26 19:10:58 +0000

    decent

--- src/App.js
+++ src/App.js
@@ -22,8 +22,6 @@ const App = () => {
     useEffect(() => {
         getPlacesData(bounds.ne, bounds.sw)
             .then((data) => {
-            console.log(data);
-
             setPlaces(data);
         })
     }, [coordinates, bounds ]);
@@ -42,7 +40,7 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List />
+                    <List places={places} />
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map
